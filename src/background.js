0ac74f8c2e18510108f45ce33c199585d465558e
Kevin Dang <kevindang22@gmail.com>
2019-12-28 16:40:19 -0500

    MVP pip when switching tabs

--- src/background.js
+++ src/background.js
@@ -1,6 +1,6 @@
 // Copyright 2018 Google LLC
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");1
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -13,26 +13,80 @@
 // limitations under the License.
 
 if (!document.pictureInPictureEnabled) {
-  chrome.browserAction.setTitle({ title: 'Picture-in-Picture NOT supported' });
+    chrome.browserAction.setTitle({title: 'Picture-in-Picture NOT supported'});
 } else {
-  chrome.browserAction.onClicked.addListener(tab => {
-    chrome.tabs.executeScript({ file: 'script.js', allFrames: true });
-  });
+    chrome.tabs.onActivated.addListener(activeInfo => {
+        var currentTab = -1;
+        var pipTab = -1;
+
+        chrome.storage.local.get(['currentTab', 'pipTab'], data => {
+            currentTab = data['currentTab'] || -1;
+            pipTab = data['pipTab'] || -1;
+
+            console.log(data);
+
+            console.log('tabID: ' + activeInfo.tabId);
+            console.log('currentTab: ' + currentTab);
+            console.log('pipID: ' + pipTab);
+            // console.log('windowID: ' + activeInfo.windowId);
+            var scriptTab = -2;
+            if ((pipTab === -1)) {
+                scriptTab = currentTab;
+            } else if (pipTab === activeInfo.tabId) {
+                scriptTab = pipTab
+            }
+
+            if (scriptTab !== -2) {
+                console.log('EXECUTED SCRIPT on tab: ' + scriptTab);
+                chrome.tabs.executeScript(scriptTab, {file: 'script.js', allFrames: true});
+            }
+
+            console.log('set CurrentTab: ' + activeInfo.tabId);
+            chrome.storage.local.set({'currentTab': activeInfo.tabId});
+        });
+    });
+
+    chrome.tabs.onUpdated.addListener( (tabId, changeInfo, tab) => {
+        console.log('~~tabs.onUpdated~~');
+        console.log('tabID: ' + tabId);
+        console.log('tab.id: ' + tab.id);
+        console.log('~~~~~~~~~~~~~~~~~~~');
+        chrome.storage.local.set({'currentTab': tabId});
+    });
+
+    // chrome.webNavigation.onCreatedNavigationTarget.addListener(details => {
+    //     console.log('~~webNavigation.onCreatedNavigationTarget~~');
+    //     console.log('sourceTabId: ' + details.sourceTabId);
+    //     console.log('~~~~~~~~~~~~~~~~~~~');
+    // })
 }
+
+chrome.browserAction.onClicked.addListener(tab => {
+    chrome.storage.local.clear()
+});
 
 var _gaq = _gaq || [];
 _gaq.push(['_setAccount', 'UA-134864766-1']);
 
-chrome.runtime.onMessage.addListener(data => {
-  if (data.message === 'enter')
-    _gaq.push(['_trackPageview']);
+chrome.runtime.onMessage.addListener( (request, sender, sendResponse) => {
+    if (request.message === 'enter') {
+        _gaq.push(['_trackPageview']);
+        console.log('ENTER SENDER TAB: ' + sender.tab.id);
+        chrome.storage.local.set({'pipTab': sender.tab.id});
+    } else if (request.message === 'leave') {
+        console.log('LEAVE SENDER TAB: ' + sender.tab.id);
+        chrome.storage.local.set({'pipTab': -1});
+    }
 });
 
-chrome.storage.sync.get({ optOutAnalytics: false }, results => {
-  if (results.optOutAnalytics) {
-    return;
-  }
-  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-  ga.src = 'https://ssl.google-analytics.com/ga.js';
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+chrome.storage.sync.get({optOutAnalytics: false}, results => {
+    if (results.optOutAnalytics) {
+        return;
+    }
+    var ga = document.createElement('script');
+    ga.type = 'text/javascript';
+    ga.async = true;
+    ga.src = 'https://ssl.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(ga, s);
 });
--- src/script.js
+++ src/script.js
@@ -13,28 +13,34 @@
 // limitations under the License.
 
 (async () => {
-  const videos = Array.from(document.querySelectorAll('video'))
-    .filter(video => video.readyState != 0)
-    .filter(video => video.disablePictureInPicture == false)
-    .sort((v1, v2) => {
-      const v1Rect = v1.getClientRects()[0];
-      const v2Rect = v2.getClientRects()[0];
-      return ((v2Rect.width * v2Rect.height) - (v1Rect.width * v1Rect.height));
-    });
+    const videos = Array.from(document.querySelectorAll('video'))
+        .filter(video => video.readyState != 0)
+        .filter(video => video.disablePictureInPicture == false)
+        .filter(video => !video.paused)
+        .sort((v1, v2) => {
+            const v1Rect = v1.getClientRects()[0];
+            const v2Rect = v2.getClientRects()[0];
+            return ((v2Rect.width * v2Rect.height) - (v1Rect.width * v1Rect.height));
+        });
 
-  if (videos.length === 0)
-    return;
+    console.log(videos.length);
 
-  const video = videos[0];
+    if (videos.length === 0)
+        return;
 
-  if (video.hasAttribute('__pip__')) {
-    await document.exitPictureInPicture();
-  } else {
-    await video.requestPictureInPicture();
-    video.setAttribute('__pip__', true);
-    video.addEventListener('leavepictureinpicture', event => {
-      video.removeAttribute('__pip__');
-    }, { once: true });
-    chrome.runtime.sendMessage({ message: 'enter' });
-  }
+    const video = videos[0];
+
+    if (video.hasAttribute('__pip__')) {
+        await document.exitPictureInPicture();
+        chrome.runtime.sendMessage({message: 'leave'});
+    } else {
+        console.log('REQUESTED PIP')
+        await video.requestPictureInPicture();
+        video.setAttribute('__pip__', true);
+        video.addEventListener('leavepictureinpicture', event => {
+            video.removeAttribute('__pip__');
+        }, {once: true});
+
+        chrome.runtime.sendMessage({message: 'enter'});
+    }
 })();
