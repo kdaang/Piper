0c5b953de698e2e3cdc5cac672cd48618bf43962
Kevin Dang <kevindang22@gmail.com>
2020-01-05 11:25:16 -0500

    playground

--- src/background.js
+++ src/background.js
@@ -12,78 +12,81 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+function pipTab(activeInfo) {
+    var lastTabs = null;
+    var pipTab = null;
+
+    chrome.storage.local.get(['lastTab', 'pipTab'], data => {
+        let windowIdKey = activeInfo.windowId.toString();
+        lastTabs = data['lastTab'] || null;
+        pipTab = data['pipTab'] || null;
+
+        if (lastTabs != null) {
+            for (var key in lastTabs) {
+                console.log('lastTabId: ' + lastTabs[key] + ' lastTabWindowId: ' + key);
+            }
+        }
+        if (activeInfo != null) {
+            console.log('currentTabId: ' + activeInfo.tabId + ' currentTabWindowId: ' + activeInfo.windowId);
+        }
+        if (pipTab != null) {
+            console.log('pipTabId: ' + pipTab.tabId + ' pipTabWindowId: ' + pipTab.windowId);
+        }
+
+        var lastTab = null;
+        if (lastTabs != null && windowIdKey in lastTabs) {
+            lastTab = {'tabId': lastTabs[windowIdKey], 'windowId': activeInfo.windowId};
+        }
+
+        // if (lastTab != null && lastTab.tabId === activeInfo.tabId && lastTab.windowId === activeInfo.windowId) {
+        //     return;
+        // }
+
+        var scriptTabId = -2;
+        if ((pipTab == null) && lastTab != null && lastTab.windowId === activeInfo.windowId) {
+            scriptTabId = lastTab.tabId;
+        } else if (pipTab != null && pipTab.tabId === activeInfo.tabId && pipTab.windowId === activeInfo.windowId) {
+            scriptTabId = pipTab.tabId
+        }
+
+        if (scriptTabId !== -2) {
+            chrome.tabs.query({'pinned': false, 'windowId': activeInfo.windowId}, tabs => {
+                for (var tab of tabs) {
+                    // console.log('LOOP TABS: ' + tab.id + 'windowID: ' + tab.windowId);
+                    if (tab.id === scriptTabId) {
+                        console.log('EXECUTED SCRIPT on tab: ' + scriptTabId);
+                        chrome.tabs.executeScript(scriptTabId, {file: 'script.js', allFrames: true});
+                        break;
+                    }
+                }
+            });
+        }
+    });
+}
+
 if (!document.pictureInPictureEnabled) {
     chrome.browserAction.setTitle({title: 'Picture-in-Picture NOT supported'});
 } else {
     chrome.tabs.onActivated.addListener(activeInfo => {
-        var lastTabs = null;
-        var pipTab = null;
+        pipTab(activeInfo);
 
-        chrome.storage.local.get(['lastTab', 'pipTab'], data => {
+        chrome.storage.local.get(['lastTab'], data => {
             let windowIdKey = activeInfo.windowId.toString();
-            lastTabs = data['lastTab'] || null;
-            pipTab = data['pipTab'] || null;
+            var newLastTabs = data['lastTab'] || {};
 
-            console.log(pipTab == null);
-
-            if (lastTabs != null) {
-                for (var key in lastTabs) {
-                    console.log('lastTabId: ' + lastTabs[key] + ' lastTabWindowId: ' + key);
-                }
-            }
-            if (activeInfo != null) {
-                console.log('currentTabId: ' + activeInfo.tabId + ' currentTabWindowId: ' + activeInfo.windowId);
-            }
-            if (pipTab != null) {
-                console.log('pipTabId: ' + pipTab.tabId + ' pipTabWindowId: ' + pipTab.windowId);
-            }
-
-            var lastTab = null;
-            if (lastTabs != null && windowIdKey in lastTabs) {
-                lastTab = {'tabId': lastTabs[windowIdKey], 'windowId': activeInfo.windowId};
-            }
-
-            if (lastTab != null && lastTab.tabId === activeInfo.tabId && lastTab.windowId === activeInfo.windowId) {
-                return;
-            }
-
-            var scriptTabId = -2;
-            if ((pipTab == null) && lastTab != null && lastTab.windowId === activeInfo.windowId) {
-                scriptTabId = lastTab.tabId;
-            } else if (pipTab != null && pipTab.tabId === activeInfo.tabId && pipTab.windowId === activeInfo.windowId) {
-                scriptTabId = pipTab.tabId
-            }
-
-            if (scriptTabId !== -2) {
-                chrome.tabs.query({'pinned': false, 'windowId': activeInfo.windowId}, tabs => {
-                    for (var tab of tabs) {
-                        if (tab.id === scriptTabId) {
-                            console.log('EXECUTED SCRIPT on tab: ' + scriptTabId);
-                            chrome.tabs.executeScript(scriptTabId, {file: 'script.js', allFrames: true});
-                            break;
-                        }
-                    }
-                });
-            }
-
-            chrome.storage.local.get(['lastTab'], data => {
-                let windowIdKey = activeInfo.windowId.toString();
-                var newLastTabs = data['lastTab'] || {};
-
-                if (windowIdKey in newLastTabs) {
+            if (windowIdKey in newLastTabs) {
+                data['lastTab'][windowIdKey] = activeInfo.tabId;
+                chrome.storage.local.set(data);
+            } else {
+                if (Object.keys(newLastTabs).length === 0) {
+                    var newEntry = {};
+                    newEntry[windowIdKey] = activeInfo.tabId;
+                    chrome.storage.local.set({'lastTab': newEntry});
+                } else {
                     data['lastTab'][windowIdKey] = activeInfo.tabId;
                     chrome.storage.local.set(data);
-                } else {
-                    if (Object.keys(newLastTabs).length === 0) {
-                        var newEntry = {};
-                        newEntry[windowIdKey] = activeInfo.tabId;
-                        chrome.storage.local.set({'lastTab': newEntry});
-                    } else {
-                        data['lastTab'][windowIdKey] = activeInfo.tabId;
-                        chrome.storage.local.set(data);
-                    }
                 }
-            });
+            }
         });
     });
 }
@@ -113,8 +116,28 @@ chrome.windows.onRemoved.addListener(windowId => {
     });
 });
 
+// chrome.windows.onFocusChanged.addListener(windowId => {
+//     console.log('onFocusChanged: ' + windowId);
+//
+//     chrome.windows.getLastFocused({windowTypes: ['normal', 'popup']}, window => {
+//         if (window.state === 'minimized') {
+//             console.log('onFocusChanged');
+//             chrome.storage.local.get(['lastTab'], data => {
+//                 let windowIdKey = window.id.toString();
+//                 var lastTabs = data['lastTab'] || null;
+//
+//                 console.log('PIPTABBED');
+//
+//                 pipTab({'tabId': lastTabs[windowIdKey], 'windowId': window.id});
+//             });
+//         }
+//     });
+//
+// });
+
 chrome.browserAction.onClicked.addListener(tab => {
-    chrome.storage.local.clear()
+    chrome.storage.local.clear();
+    // chrome.tabs.executeScript({file: 'script.js', allFrames: true});
 });
 
 chrome.runtime.onMessage.addListener( (request, sender, sendResponse) => {
--- src/script.js
+++ src/script.js
@@ -23,19 +23,21 @@
             return ((v2Rect.width * v2Rect.height) - (v1Rect.width * v1Rect.height));
         });
 
-    console.log(videos.length);
-
     if (videos.length === 0)
         return;
 
     const video = videos[0];
 
     if (video.hasAttribute('__pip__')) {
-        await document.exitPictureInPicture();
+        await document.exitPictureInPicture().catch(error => {
+            console.log('FAILED: ' + error)
+        });
         chrome.runtime.sendMessage({message: 'leave'});
     } else {
         console.log('REQUESTED PIP')
-        await video.requestPictureInPicture();
+        await video.requestPictureInPicture().catch(error => {
+            console.log('FAILED: ' + error)
+        });
         video.setAttribute('__pip__', true);
         video.addEventListener('leavepictureinpicture', event => {
             video.removeAttribute('__pip__');
