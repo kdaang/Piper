debdecfdda18b513120e48a0da0e5b4fdc7898be
Kevin Dang <kevindang22@gmail.com>
2021-01-27 09:48:20 -0500

    clean

--- src/script.js
+++ src/script.js
@@ -1,17 +1,3 @@
-// Copyright 2018 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 (async () => {
     const videos = Array.from(document.querySelectorAll('video'))
         .filter(video => video.readyState != 0)
@@ -27,6 +13,9 @@
         return;
 
     const video = videos[0];
+
+    console.log(window);
+    console.log(window.navigator);
 
     if (video.hasAttribute('__pip__')) {
         await document.exitPictureInPicture().catch(error => {
